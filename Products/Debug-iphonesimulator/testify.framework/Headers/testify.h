23f1c5f4c30eb814e24616075c177a5ca3588b8a
Haroon <haroon5507@gmail.com>
2024-06-03 16:18:36 +0500

    first commit

---
+++ Products/Debug-iphonesimulator/testify.framework/Headers/testify.h
@@ -0,0 +1,18 @@
+//
+//  testify.h
+//  testify
+//
+//  Created by Haroon Shoukat on 03/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testify.
+FOUNDATION_EXPORT double testifyVersionNumber;
+
+//! Project version string for testify.
+FOUNDATION_EXPORT const unsigned char testifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testify/PublicHeader.h>
+
+
---
+++ testify/Validator.swift
@@ -0,0 +1,14 @@
+//
+//  Validator.swift
+//  testify
+//
+//  Created by Haroon Shoukat on 03/06/2024.
+//
+
+import Foundation
+
+struct Validator {
+    static func getDate(){
+        print("@@##$#$#$#$#$3434343434")
+    }
+}
